e988966c7936f7f371690751706e84a8ed12b23f
markfan <fanzhengchen@gmail.com>
2018-02-11 02:29:29 +0800

    modify cmake

--- opengl.cpp
+++ opengl.cpp
@@ -16,6 +16,9 @@ int OpenGL::init(GLFWkeyfun key_callback) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     // Create a GLFWwindow object that we can use for GLFW's functions
