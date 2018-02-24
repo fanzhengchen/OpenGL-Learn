897b83643fdd3568d07d7ff6761ade0f541dff8f
markfan <fanzhengchen@gmail.com>
2018-02-24 21:57:11 +0800

    basic lighting materials

---
+++ Materials/cmake-build-debug/CMakeFiles/Lighting.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lighting.dir/BasicLighting.cpp.o"
+  "CMakeFiles/Lighting.dir/Shader.cpp.o"
+  "Lighting.pdb"
+  "Lighting"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lighting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
