753e7c0b76652ec8091e6d1d8e65860fb9965f64
markfan <fanzhengchen@gmail.com>
2018-03-01 11:11:18 +0800

    model load

---
+++ Model/cmake-build-debug/CMakeFiles/Lighting.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/SOIL2/SOIL2.c" "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/cmake-build-debug/CMakeFiles/Lighting.dir/SOIL2/SOIL2.c.o"
+  "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/SOIL2/etc1_utils.c" "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/cmake-build-debug/CMakeFiles/Lighting.dir/SOIL2/etc1_utils.c.o"
+  "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/SOIL2/image_DXT.c" "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/cmake-build-debug/CMakeFiles/Lighting.dir/SOIL2/image_DXT.c.o"
+  "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/SOIL2/image_helper.c" "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/cmake-build-debug/CMakeFiles/Lighting.dir/SOIL2/image_helper.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../SOIL2"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/Shader.cpp" "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/cmake-build-debug/CMakeFiles/Lighting.dir/Shader.cpp.o"
+  "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/main.cpp" "/Users/fanzhengchen/CLionProjects/OpengDemos/Model/cmake-build-debug/CMakeFiles/Lighting.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SOIL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
